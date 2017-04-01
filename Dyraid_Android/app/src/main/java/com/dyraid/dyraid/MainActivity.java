431060dbc6fbb1a81f9844a14f939f3434938903
emekaokoro <emeka.okoro.00@gmail.com>
2017-04-01 16:32:58 -0500

    start login

--- Dyraid_Android/app/src/main/java/com/dyraid/dyraid/MainActivity.java
+++ Dyraid_Android/app/src/main/java/com/dyraid/dyraid/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_MESSAGE + 'd', message);
         startActivity(intent);
     }
 }
