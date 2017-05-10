c243cd5cca33d794e4d74fa33ba6ef34acc51179
emekaokoro <emeka.okoro.00@gmail.com>
2017-05-10 02:34:53 -0500

    login and display list of meal logs

--- Dyraid_Android/app/src/main/java/com/dyraid/dyraid/MainActivity.java
+++ Dyraid_Android/app/src/main/java/com/dyraid/dyraid/MainActivity.java
@@ -1,10 +1,13 @@
 package com.dyraid.dyraid;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -15,8 +18,10 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.dyraid.MESSAGE";
     private static final String USERLOGLIST_REQUEST_URL = "http://192.168.56.56:8000/userlog/api/";//Remove hardcode
 
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView mTextView = (TextView) findViewById(R.id.mTextView);
         final EditText editText = (EditText) findViewById(R.id.editText);
+        final ListView listView = (ListView) findViewById(R.id.mLogListView);
 
         //create new Request
         // Instantiate the RequestQueue.
@@ -45,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         mTextView.setText("Response is: "+ response);
+
+                        ArrayAdapter<String> adapter;
+                        ArrayList<String> items = new ArrayList<String>();
+                        try
+                        {
+                            JSONArray responseArray = new JSONArray(response);
+                            for (int i = 0; i < responseArray.length(); i++) {
+                                items.add(responseArray.getJSONObject(i).toString());
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+
+                        // adapter = new ArrayAdapter(this, R.layout.item_layout,R.id.txt,items);
+                        adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.item_layout,R.id.txt,items);
+                        listView.setAdapter(adapter);
                     }
                 },
                 new Response.ErrorListener() {
